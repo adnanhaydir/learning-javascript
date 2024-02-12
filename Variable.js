ce729e3b649425006e30e95da808b5ee79e690f4
adnanhaydir <adnanhaydirwork@gmail.com>
2024-02-12 15:37:08 +0700

    Learning : Variable

---
+++ Variable.js
@@ -0,0 +1,16 @@
+// VARIABLE
+
+var fullName = "Adnan Haydir Prayudi"; // NOT RECOMMENDED USING VAR
+console.log(fullName);
+
+fullName = "Adnan Prayudi";
+console.log(fullName);
+
+let firstName = "Adnan"; // LET : YOU CAN CHANGE THE VALUE
+console.log(firstName);
+
+firstName = "Haydir";
+console.log(firstName);
+
+const lastName = "Prayudi"; // CONST : UNABLE TO CHANGE VALUE
+console.log(lastName);
